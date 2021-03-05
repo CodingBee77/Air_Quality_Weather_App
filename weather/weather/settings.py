4bc2df089ca5d3dba161c2c7d4f0dd52220c527a
CodingBee77 <00martyna.znajdek00@gmail.com>
2021-03-05 18:57:44 +0100

    Dockerized Air quality weather app

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -31,7 +31,7 @@ AIRLY_API_KEY = os.environ.get('AIRLY_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 
 
 # Application definition
